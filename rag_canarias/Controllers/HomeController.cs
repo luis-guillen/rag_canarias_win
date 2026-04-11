89b0f07e4ee8685d468d27baa7377dc0fdbf18d8
luis-guillen <luisgservsp@gmail.com>
2026-04-11 14:14:54 +0200

    feat: add web crawler interface and controller action to fetch and save HTML content

--- rag_canarias/Controllers/HomeController.cs
+++ rag_canarias/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
 
 namespace rag_canarias.Controllers
 {
@@ -25,6 +28,33 @@ namespace rag_canarias.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Crawl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                return Content("URL vacía");
+            }
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var html = await client.GetStringAsync(url).ConfigureAwait(false);
+
+                    // Asegurar que la carpeta exista
+                    System.IO.Directory.CreateDirectory(@"C:\temp");
+                    System.IO.File.WriteAllText(@"C:\temp\pagina.txt", html);
+
+                    return Content("Página descargada y guardada en C:\\temp\\pagina.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content("Error: " + ex.Message);
+            }
         }
     }
 }
